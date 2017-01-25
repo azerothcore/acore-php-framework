e7a998ff89dbc24f8cb3fb60817cf1a0389e5781
Yehonal <yehonal.azeroth@gmail.com>
2017-01-25 16:20:14 +0100

    Fixed sendItem soap command

--- game-mail/src/MailSoap.php
+++ game-mail/src/MailSoap.php
@@ -6,8 +6,12 @@ use \ACore\Soap\SoapModule;
 
 class MailSoap extends SoapModule {
 
-    public function sendItem($player, $object, $message, $itemId, $stack) {
-        return $this->executeCommand('.send items  ' . $player . ' ".' . $object . '" "' . $message . '" .' . $itemId . ':' . $stack);
+    public function sendItem($playerName, $subject, $message, $itemId, $stack) {
+        $_message = addslashes($message);
+        $_subject = addslashes($subject);
+        $_itemId = intval($itemId);
+        $_stack = intval($stack);
+        return $this->executeCommand('.send items  ' . $playerName . ' "' . $_subject . '" "' . $_message . '" ' . $_itemId . ':' . $_stack);
     }
 
 }
